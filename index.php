175fb3e00d30ff36eeef605335bad8c3d909043e
LuisNoe21 <luisnoe.rodriguez09@gmail.com>
2022-11-05 14:24:28 -0600

    landing page

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rapi Mandaditos</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <header class="hero">
+        <div class="textos-hero">
+            <h1>Rapi Mandaditos</h1>
+            <p>Ofrecemos un Servicio Rápido y de Calidad!</p>
+            <a href="#contacto">Contáctanos dando click</a>
+        </div>
+        <div class="svg-hero" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+                style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                    style="stroke: none; fill: #fff;"></path>
+            </svg></div>
+    </header>
+
+
+    <section class="wave-contenedor website">
+        <img src="img/rapi.svg" alt="">
+        <div class="contenedor-textos-main">
+            <h2 class="titulo left">No te Compliques. Te lo llevamos ya!
+            </h2>
+            <p class="parrafo">Valoramos su tiempo por esta razon tratamos de brindar un servicio de excelente calidad, garantizando recolección y entrega en el lugar y hora que el cliente necesita, es la forma en que pagamos esta confianza.!</p>
+            <!-- <a href="" class="cta">Learn more</a> -->
+        </div>
+    </section>
+
+    <section class="info">
+        <div class="contenedor">
+            <h2 class="titulo left">Nuestro Destino es tu Confianza</h2>
+            <p>Más rápido y mejor.</p>
+        </div>
+    </section>
+
+    <section class="cards contenedor">
+        <h2 class="titulo">Nuestros Servicios </h2>
+        <div class="content-cards">
+            <!-- <article class="card">
+                <i class="fas fa-chess"></i>
+                <h3>Title Card</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p> -->
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+            <article class="card">
+                <i class="far fa-clone"></i>
+                <h3>Contratos Empresariales</h3>
+                <p>Acuerdo entre partes por la obtención de servicios.</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+            <article class="card">
+                <i class="fas fa-donate"></i>
+                <h3>Trámites Bancarios</h3>
+                <p>Realizamos por ud cualquier tipo de gestión bancaria en todas las sucursales de la zona.</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+            <article class="card">
+                <i class="fas fa-book"></i>
+                <h3>Movimiento de Papeleria</h3>
+                <p>Le transladamos su papeleria al lugar que ud desee</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+            <article class="card">
+                <i class="fas fa-pizza-slice"></i>
+                <h3>Compras en Supermercados y Restaurantes</h3>
+                <p>Le llevamos hasta la puerta de su casa sus alimentos y lo necesario para la preparación de ellos.</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+            <article class="card">
+                <i class="fas fa-ambulance"></i>
+                <h3>Compras en Farmacias o en su Tienda de Preferencia</h3>
+                <p>De la manera mas rápida reciba sus medicamentos y prendas de los lugares de su preferencia.</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </article>
+        </div>
+    </section>
+
+    <!-- <section class="galeria">
+        <div class="contenedor">
+            <h2 class="titulo">Our work</h2>
+            <article class="galeria-cont">
+                <img src="img/uno.jpg" alt="">
+                <img src="img/dos.jpg" alt="">
+                <img src="img/tres.jpg" alt="">
+                <img src="img/cuatro.jpg" alt="">
+                <img src="img/cinco.jpg" alt="">
+                <img src="img/seis.jpg" alt="">
+            </article>
+        </div>
+    </section>
+
+    <section class="info-last"> -->
+
+        <div class="contenedor last-section">
+            <div class="contenedor-textos-main">
+                <h2 class="titulo left">Tan pronto como lo pidas!</h2>
+                <p class="parrafo">Entrega con profesionalismo.</p>
+                <!-- <a href="" class="cta">Learn more</a> -->
+            </div>
+            <img src="img/rapi2.svg" alt="">
+        </div>
+        
+        <div class="svg-wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+            style="height: 100%; width: 100%;">
+            <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                style="stroke: none; fill: #f5576c;"></path>
+        </svg></div>
+    </section>
+
+    <!-- <footer id="contacto">
+        <div class="contenedor">
+            <h2 class="titulo">Contact us</h2>
+            <form action="" class="form">
+                <input class="input"  type="text" name="" id="" placeholder="Nombre">
+                <input class="input"  type="email" name="" id="" placeholder="Email">
+                <textarea  class="input" name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                <input class="input"  type="submit" value="Enviar">
+            </form>
+        </div>
+    </footer> -->
+
+    <footer id="contacto">
+        <h2 style="text-align: center;" class="titulo left">Contactanos!</h2>
+        <div class="contenedor-footer">
+            
+            <div class="content-foo">
+                <h4>Llámenos</h4>
+                <p>Cel: 3387-5576</p>
+                <p>Cel: 9391-4462</p>
+            </div>
+            <div class="content-foo">
+                <h4>Whatsapp</h4>
+                <p>Dale clic  <i  class="far fa-hand-point-down"></i></p>
+                <a href="mailto:luisnoe.rodriguez09@gmail.com?Subject=Interesado%20en%20el%20servicio"><i style="font-size: 35px; color: black; " class="far fa-envelope-open"></i></a> 
+            </div>
+           
+            <div class="content-foo">
+                <h4>Whatsapp</h4>
+
+                <p>Dale clic  <i  class="far fa-hand-point-down"></i></p>
+               <a href="https://wa.me/+50433875576?text=Hola,%20me%20gustar%C3%ADa%20contratar%20sus%20servicios!%20
+               "><i style="font-size: 40px;color:darkgreen " class="fa fa-whatsapp"></i></a> 
+                
+            </div>
+        </div>
+        <h2 class="titulo-final">&copy; Desarrollado por Luis Noe Rodriguez | Todos los derechos reservados 2022. </h2>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/c15b744a04.js" crossorigin="anonymous"></script>
+</body>
+<a href="mailto:rafa@gmail.com?Subject=Interesado%20en%20el%20curso">Contactar por correo</a>
+</html>
